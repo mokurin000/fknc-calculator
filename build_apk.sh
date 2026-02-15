d7dc0bac44d84ed51544cfe9e0a8865c0975e743
莯凛 <mokurin000@gmail.com>
2026-02-15 18:50:25 +0800

    fix: js script to ensure redirection works on Android

--- build_apk.sh
+++ build_apk.sh
@@ -3,5 +3,6 @@
 cargo tauri android build \
     --apk \
     --split-per-abi \
-    --target armv7,aarch64
+    --target armv7 \
+    --target aarch64
 mv src-tauri/gen/android/app/build/outputs/apk/*/release/*-release.apk .
