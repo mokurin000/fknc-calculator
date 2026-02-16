57e5df84dd79adc009e4709dfca8d21f3aa905c8
mokurin000 <mokurin000@gmail.com>
2026-02-16 22:15:09 +0800

    chore: load offline resources

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,7 @@
-use tauri::Manager;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .append_invoke_initialization_script(include_str!("index.js"))
-        .setup(|app| {
-            let webview = app.webview_windows().into_values().next().unwrap();
-            webview.eval("window.location.replace('https://www.fknc.top')")?;
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
