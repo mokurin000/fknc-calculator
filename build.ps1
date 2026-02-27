6cea580092f6095c1a2c2f7400b416a38319dc36
莯凛 <mokurin000@gmail.com>
2026-02-28 02:43:29 +0800

    build: fix bundle script

--- build.ps1
+++ build.ps1
@@ -5,5 +5,5 @@ cargo tauri android build `
     --target aarch64 `
     --target x86_64 `
     --split-per-abi
-Move-Item src-tauri/gen/android/app/build/outputs/apk/*/release/*-release.apk .
-Move-Item src-tauri/target/release/bundle/nsis/*-setup.exe .
+Move-Item -Force src-tauri/gen/android/app/build/outputs/apk/*/release/*-release.apk .
+Move-Item -Force src-tauri/target/release/bundle/nsis/*-setup.exe .
