62b99404439311850422827333af3cd83c64952f
莯凛 <mokurin000@gmail.com>
2026-02-28 05:57:08 +0800

    chore: bump ver to 0.2.4

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "top.fknc"
         minSdk = 24
         targetSdk = 34
-        versionCode = tauriProperties.getProperty("tauri.android.versionCode", "7").toInt()
-        versionName = tauriProperties.getProperty("tauri.android.versionName", "0.2.3")
+        versionCode = tauriProperties.getProperty("tauri.android.versionCode", "8").toInt()
+        versionName = tauriProperties.getProperty("tauri.android.versionName", "0.2.4")
     }
     signingConfigs { 
           create("release") { 
