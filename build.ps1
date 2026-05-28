4bdf45f74924db0379d9355d46758d011f5d29c6
莯凛 <mokurin000@gmail.com>
2026-05-28 17:16:04 +0800

    build: no x86_64 build by default

--- build.ps1
+++ build.ps1
@@ -3,7 +3,6 @@ cargo tauri build -b nsis
 cargo tauri android build `
     --apk `
     --target aarch64 `
-    --target x86_64 `
     --split-per-abi
 Move-Item -Force src-tauri/gen/android/app/build/outputs/apk/*/release/*-release.apk .
 Move-Item -Force src-tauri/target/release/bundle/nsis/*-setup.exe .
